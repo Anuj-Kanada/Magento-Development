e5e32db28d9790a7b2ba5f17a004c15fefbfc088
Anuj Kanada <128421858+Anuj-Kanada@users.noreply.github.com>
2024-07-04 14:36:18 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created By : Rohan Hapani
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Brainvire_GridOption2',
+    __DIR__
+);
